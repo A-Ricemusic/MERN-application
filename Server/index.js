70f08b5f2a2452d800eea8b48f59027f9fcb90d0
A-Ricemusic <108903641+A-Ricemusic@users.noreply.github.com>
2022-11-10 15:57:41 -0500

    Need to troubleshoot require method

--- Server/index.js
+++ Server/index.js
@@ -1,15 +1,34 @@
-//get dependancies
-//npm modules needed are express,cors,mongoose,dotenv
+//npm external modules needed are express,cors,mongoose,dotenv
+
+//get base dependancies
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
 
+//app creation and base use statements for external dependancies
 
-//local variables
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+//local variables and path variables setup
 const portNumber =process.env.PORTNUMBER || 5000;
 const mongoDbUri = process.env.mongoDbUri
 const dbConnection = mongoose.connection;
+const routesDirs = process.env.routesDirs
+const modelsDirs = process.env.modelsDirs
+
+const pathConfig = {
+    'routeUser': `${routesDirs}/userRoute`,
+
+    'routeFood': `${routesDirs}/foodRoute`
+}
+
+
+//unable able to find for some reason not sure why
+//const userRouter = require(pathConfig['routeUser']);
+//const foodRouter = require(pathConfig['routeFood']);
 
 //database connection Mongo DB
 mongoose.connect(mongoDbUri);
@@ -17,18 +36,15 @@ dbConnection.once('open', () => {
     console.log("MongoDB connection sucessful");
 });
 
-//URL Routing configuartion
-// const userRouter = require('./routes/routeuser');
-// const foodRouter = require('./routes/routefood');
-
-// app.use('/user',userRouter)
+//URL Routing configuartion, and use statements
+//app.use('/user',userRouter)
 // app.use('/food', foodRouter)
 
 
 
 //Starts Server
-
-
 app.listen(portNumber, () =>{
 console.log(`Server is running ${portNumber}`)
+console.log(pathConfig['routeUser'])
+console.log(pathConfig['routeFood'])
 });  
