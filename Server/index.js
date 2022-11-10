d8f13b6d06c06f4f0e0bad40cef6d5c1659e02b7
A-Ricemusic <108903641+A-Ricemusic@users.noreply.github.com>
2022-11-10 13:00:59 -0500

    Data connection sucessful

--- Server/index.js
+++ Server/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose')
-
-mongoose.connect('mongodb+srv://A_Ricemusic:A_Ricemusic123@cluster0.0zixhf2.mongodb.net/?retryWrites=true&w=majority')
 const app = express();
 
-app.listen(300, () =>{
+
+mongoose.connect('mongodb+srv://A_RicemusicEspc:A_RicemusicEspc123@cluster0.s9ibflx.mongodb.net/?retryWrites=true&w=majority');
+
+const dbConnection = mongoose.connection;
+
+dbConnection.once('open', () => {
+    console.log("MongoDB connection sucessful");
+});
+
+
+app.listen(3000, () =>{
 console.log("Server Starts")
 });  
